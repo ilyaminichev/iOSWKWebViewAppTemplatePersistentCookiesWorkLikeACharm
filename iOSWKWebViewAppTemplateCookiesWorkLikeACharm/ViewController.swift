2f1411b5af8ba1a50aba44eea7328446fdd0a558
ilyaminichev <ilya.minichev@gmail.com>
2021-08-18 05:09:22 +0300

    Initial Commit

---
+++ iOSWKWebViewAppTemplateCookiesWorkLikeACharm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSWKWebViewAppTemplateCookiesWorkLikeACharm
+//
+//  Created by Ilya Minichev on 18.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
